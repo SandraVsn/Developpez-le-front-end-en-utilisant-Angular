4cb7624fb5e9b23c3d524d3fed5731f3eeb81e85
Sandra VOISIN <sandra.voisin92@gmail.com>
2023-10-30 07:56:11 +0100

    Improve fetch error and redirect to error Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { DetailsComponent } from './pages/details/details.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'not-found',
     component: NotFoundComponent,
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
   },
   {
     path: ':country',
